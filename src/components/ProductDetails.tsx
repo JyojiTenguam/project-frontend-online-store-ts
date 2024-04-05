7b363007531b3d56faf7b38b7de191522c382cf8
David Augusto <david.lm.augusto@gmail.com>
2024-04-05 20:08:13 -0300

    requisito 9 concluido

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { Product } from '../pages/Home';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Product } from '../types';
 import { getProductById } from '../services/api';
 
 function ProductDetails() {
   const [product, setProduct] = useState<Product>();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchProduct() {
@@ -14,6 +15,24 @@ function ProductDetails() {
     }
     fetchProduct();
   }, [id]);
+
+  const handleAddClick = async (
+    title: string,
+    thumbnail: string,
+    price: number,
+  ) => {
+    const existingProductsJSON = localStorage.getItem('products');
+    let existingProducts: any[] = [];
+
+    if (existingProductsJSON) {
+      existingProducts = JSON.parse(existingProductsJSON);
+    }
+
+    const newProduct = { id, title, thumbnail, price };
+    existingProducts.push(newProduct);
+
+    localStorage.setItem('products', JSON.stringify(existingProducts));
+  };
 
   return (
     <div>
@@ -26,11 +45,26 @@ function ProductDetails() {
             data-testid="product-detail-image"
           />
           <p data-testid="product-detail-price">{`R$ ${product.price}`}</p>
-          <Link to="/shopping-cart" data-testid="shopping-cart-button">
-            <button>
-              Carrinho
+          <p>
+            <button
+              data-testid="shopping-cart-button"
+              onClick={ () => navigate('/shopping-cart') }
+            >
+              Carrinho de Compras
             </button>
-          </Link>
+          </p>
+          <p>
+            <button
+              data-testid="product-detail-add-to-cart"
+              onClick={ () => handleAddClick(
+                product.title,
+                product.thumbnail,
+                product.price,
+              ) }
+            >
+              Adicionar ao Carrinho
+            </button>
+          </p>
         </div>
       )}
     </div>
